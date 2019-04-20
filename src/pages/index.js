f78b44578c45caf65b43b880bf5d78b97a940578
da crands <davecrands@gmail.com>
2019-04-20 11:39:11 -0400

    /index redesign to two col layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,35 +6,39 @@ import Footer from '../components/footer'
 
 
 
-const IndexPage = ({data}) => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`David Crandall`, `web developer`, `react`]} />
     {/* <Navbar />     */}
-    <header className="header"> 
-      <div className="container">
-        <h1>David's Blog</h1>        
-        <p className="">I am <a href="https://crandall.dev" target="_blank" rel="noopener noreferrer">David Crandall</a> and 
-        this is where I write about programming.</p>      
-      </div>    
-    </header> 
-      <div className="pages">
+    <div className="flex-center">
+      <header className="header header--flex">
+        <div>
+        <h1>David Crandall</h1>
+        <h3>Full Stack Web Developer</h3>
+        <p className="content">I am {` `} 
+        <a href="https://crandall.dev" target="_blank" rel="noopener noreferrer"> David Crandall</a> {` `} 
+        and this is where I write about programming. You may find the code for most of my projects
+        on {` `} <a href="https://github.com/dacrands">my github.</a></p>
+        </div>
+      </header>
+      <nav className="pages">
         {
           data.allMarkdownRemark.edges.map(post => {
             return <Link className="pages__link"
               to={post.node.frontmatter.path}
             >
               <div className="pages__link-title">
-                <h3>{post.node.frontmatter.title}</h3>
+                <h4>{post.node.frontmatter.title}</h4>
                 <small>{post.node.frontmatter.date}</small>
-              </div>                
+              </div>
               <div className="pages__link-desc">
                 <p>{post.node.frontmatter.info}</p>
-              </div>              
-              </Link>
+              </div>
+            </Link>
           })
-        }    
-      </div>        
-      <Footer/>     
+        }
+      </nav>
+    </div>
   </Layout>
 )
 
